e1fda6b6edd8a5a2994fac79bbaf13371bb54d56
yeeyee69 <50676238+yeeyee69@users.noreply.github.com>
2020-01-30 11:37:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 $("#heading").dblclick(function() {
     alert("You live here I let you live here I gave you a home Now you live here You're ungrateful I loved u like a person");
+    console.log("gIgNQD85vJY (1)")
+    console.log("83JBVgrKhYY (3)")
 });
